6f17e7e80ce9d1d10c4c9c5aae5e414faae19b14
Michael-coding06 <vietkhoaktk@gmail.com>
2026-01-02 12:23:56 +0700

    fix package file

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://3.27.131.69:5000/",
+    // baseURL: "http://3.27.131.69:5000/",
+    baseURL: "/",
     // baseURL: process.env.REACT_APP_BACKEND_URL,
     withCredentials: true,
 })
